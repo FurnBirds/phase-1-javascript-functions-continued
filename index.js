04090070a09b504715f6e0bcd5ef4ab244e48b01
Steven FU <furnbirds@gmail.com>
2024-02-04 15:55:52 +0800

    creates 3 functions, uses default parameters and returns an inner function

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+//saturdayFun("study")
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(v = "*") {
+    
+    return function(x = `special`){
+        return(`You are ${v}${x}${v}!` )
+    }
+    
+};
+
+console.log(wrapAdjective("%")("a dedicated programmer"))
